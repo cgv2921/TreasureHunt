d832c89824824f35b9a772f7f657eeff3fd53f07
Steven Greenberg <sjg8961@rit.edu>
2017-04-14 11:53:34 -0400

    More progress

--- app/src/main/java/com/rit/se/treasurehuntvuz/FindTreasureActivity.java
+++ app/src/main/java/com/rit/se/treasurehuntvuz/FindTreasureActivity.java
@@ -7,14 +7,10 @@ import android.location.Location;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
 import android.util.Log;
-import android.graphics.Canvas;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+
+import java.text.FieldPosition;
+import java.text.MessageFormat;
 
 // Jeffrey Haines 3/4/17
 //    Made FindTreasureActivity a single instance so we can kill it when player finds all the treasure etc.
@@ -23,8 +19,6 @@ import android.graphics.BitmapFactory;
 public class FindTreasureActivity extends AppCompatActivity {
 
     public static Activity findTreasureActivity;
-    private static Location currentLocation;
-    public static String[] locationMesseges = {"Cold", "Hot", "Getting Hot", "Getting Colder"};
 
     //http://stackoverflow.com/questions/14295150/how-to-update-a-textview-in-an-activity-constantly-in-an-infinite-loop
     @Override
@@ -36,13 +30,46 @@ public class FindTreasureActivity extends AppCompatActivity {
         handler.post(new Runnable(){
             @Override
             public void run() {
-                lookForTreasure();
+                lookForTreasure(null,null);
                 handler.postDelayed(this,500); // set time here to refresh textView
             }
         });
     }
 
-    public void lookForTreasure(){
+    public void lookForTreasure(Location currentLocation, Location[] treasures){
+        float closestDis = currentLocation.distanceTo(treasures[0]);
+        Location closestLoc = treasures[0];
+        for (Location treasure : treasures){
+            if(currentLocation.distanceTo(treasure) < closestDis){
+                closestLoc = treasure;
+                closestDis = currentLocation.distanceTo(treasure);
+            }
+        }
+        float closetBearing = Math.abs(currentLocation.bearingTo(closestLoc) - currentLocation.getBearing());
+        float cloestLocation = currentLocation.distanceTo(closestLoc);
+        MessageFormat fmt = new MessageFormat("You are {0} and you are getting {1}.");
+        String bearingStr = "";
+        String distanceStr = "";
+        if (closetBearing < 15){
+            bearingStr = "blazinger";
+        } else if(closetBearing < 30){
+            bearingStr = "hoter";
+        } else if(closetBearing < 60){
+            bearingStr = "colder";
+        } else{
+            bearingStr = "frezzinger";
+        }
+        if (cloestLocation < 15){
+            distanceStr = "blazing";
+        } else if(cloestLocation < 30){
+            distanceStr = "hot";
+        } else if(cloestLocation < 60){
+            distanceStr = "cold";
+        } else{
+            distanceStr = "frezzing";
+        }
+        Object[] results = {distanceStr,bearingStr};
+        String resultStr = fmt.format(results);
 
     }
 
