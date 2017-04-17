303bf8db15397dbbc426c4218f3c23c4047528c8
HuseenMK <hjm3628@rit.edu>
2017-04-17 12:59:37 -0400

    Changed few methods in StartGameActivity.java

--- app/src/main/java/com/rit/se/treasurehuntvuz/StartGameActivity.java
+++ app/src/main/java/com/rit/se/treasurehuntvuz/StartGameActivity.java
@@ -3,65 +3,37 @@ package com.rit.se.treasurehuntvuz;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.view.View;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationServices;
 
+
 public class StartGameActivity extends AppCompatActivity implements
-        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener  {
 
-    GoogleApiClient mGoogleApiClient;
-    private String mLatitudeText;
-    private String mLongitudeText;
+    protected GoogleApiClient mGoogleApiClient;
+    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1;
 
-    public void onConnected(Bundle connectionHint) {
-        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
-            return;
-        }
-        else {
+    /**
+     * Represents a geographical location.
+     */
+    protected Location mLastLocation;
+    protected TextView mLatitudeText;
+    protected TextView mLongitudeText;
+    protected static final String TAG = "basic-location-sample";
 
-            Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(
-                    mGoogleApiClient);
-            if (mLastLocation != null) {
-                mLatitudeText = String.valueOf(mLastLocation.getLatitude());
-                mLongitudeText = String.valueOf(mLastLocation.getLongitude());
-            }
-        }
-    }
 
-    @Override
-    public void onConnectionSuspended(int i) {
-
-    }
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
-        // Create an instance of GoogleAPIClient.
-        if (mGoogleApiClient == null) {
-            mGoogleApiClient = new GoogleApiClient.Builder(this)
-                    .addConnectionCallbacks(this)
-                    .addOnConnectionFailedListener(this)
-                    .addApi(LocationServices.API)
-                    .build();
-        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_startgame);
 
@@ -102,41 +74,47 @@ public class StartGameActivity extends AppCompatActivity implements
 
         // Set resume visibility
         resumeButton.setEnabled(TreasuresSingleton.getTreasures().getResume());
+
+        // Create an instance of GoogleAPIClient.
+        buildGoogleApiClient();
     }
 
-    @Override
+    protected synchronized void buildGoogleApiClient() {
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
+    }
+
     protected void onStart() {
         mGoogleApiClient.connect();
         super.onStart();
     }
 
-    @Override
     protected void onResume() {
         super.onResume();
     }
 
-    @Override
     protected void onPause() {
         super.onPause();
     }
 
-    @Override
     protected void onStop(){
         mGoogleApiClient.disconnect();
         super.onStop();
     }
 
-    @Override
     protected void onRestart() {
         super.onRestart();
     }
 
-    @Override
     protected void onDestroy() {
         super.onDestroy();
     }
 
-    @Override
     public void onBackPressed() {
         try {
             Intent mainActivityIntent = new Intent(StartGameActivity.this, MainActivity.class);
@@ -170,8 +148,30 @@ public class StartGameActivity extends AppCompatActivity implements
 
     }
 
-    @Override
-    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+    public void onConnected(Bundle connectionHint) {
+        if (ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            if (Build.VERSION.SDK_INT >= 23) {
 
+                ActivityCompat.requestPermissions(this, new String[]{android.Manifest.permission.ACCESS_COARSE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
+            }
+
+            else {
+
+                mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+                if (mLastLocation != null) {
+                    mLatitudeText.setText(String.valueOf(mLastLocation.getLatitude()));
+                    mLongitudeText.setText(String.valueOf(mLastLocation.getLongitude()));
+                }
+            }
+        }
+    }
+
+    public void onConnectionFailed(ConnectionResult result) {
+        Log.i(TAG, "Connection failed: ConnectionResult.getErrorCode() = " + result.getErrorCode());
+    }
+
+    public void onConnectionSuspended(int i) {
+        Log.i(TAG, "Connection suspended");
+        mGoogleApiClient.connect();
     }
 }
